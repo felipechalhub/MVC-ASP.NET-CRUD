3f6d47c2f02fc1d396afc7d675326eb3417e13af
LAPTOP-62MA70CI\Felipe Portela <fp.chalhub@gmail.com>
2024-02-17 14:16:47 -0300

    Using MVC to implement page and tag helpers to navigate links

--- WebApp/Properties/Controllers/CategoriesController.cs
+++ WebApp/Properties/Controllers/CategoriesController.cs
@@ -10,14 +10,10 @@ namespace WebApp.Properties.Controllers
             var categories = CategoriesRepository.GetCategories();
             return View(categories);
         }
-        public IActionResult Edit(int? id)
+        public IActionResult Edit([FromRoute]int? id)
         {
-            var category = new Category { CategoryId = id.HasValue ? id.Value : 0 };
+            var category = CategoriesRepository.GetCategoryById(id.HasValue ? id.Value : 0);
             
-
-
-
-
             return View(category);
         }
         }
